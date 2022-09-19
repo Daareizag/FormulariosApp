fe11e7492195a955a94ff97bc6dc67001f4d7938
Daareizag <diegoareizag32074818@gmail.com>
2022-09-19 16:46:18 -0300

    Formularios reactivods (Basicos: Mostrar mensajes de error, submit del formulario) (Dinamicos: FormArray, Validaciones de los nuevos campos)

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ["",[Validators.required, Validators.minLength(3)]],
+    favorito: this.fb.array([
+      ['metal geal', Validators.required], ['dead strange', Validators.required]
+    ], Validators.required)
+  })
+  get favoritosArr() {
+    return this.miFormulario.get('favorito') as  FormArray;
   }
+  constructor(private fb: FormBuilder) { }
+  campoEsObligatorio() {
+    return this.miFormulario.controls.nombre.invalid&& this.miFormulario.controls.nombre.touched;
+  }
+  guardar() {
+    if(this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
+  }
+
+
 
 }
