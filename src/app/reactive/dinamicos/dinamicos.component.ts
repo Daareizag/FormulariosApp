edc7d33e550cf70b329840da136ca99c1bc785a3
Daareizag <diegoareizag32074818@gmail.com>
2022-09-21 16:43:58 -0300

    Agregar o eliminar datos del formulario mediante el boton "Agregar y Eliminar"

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -13,9 +13,10 @@ export class DinamicosComponent {
   miFormulario: FormGroup = this.fb.group({
     nombre: ["",[Validators.required, Validators.minLength(3)]],
     favorito: this.fb.array([
-      ['metal geal', Validators.required], ['dead strange', Validators.required]
-    ], Validators.required)
+      ['metal geal', Validators.required],
+      ['dead strange', Validators.required]], Validators.required)
   })
+  nuevoFavorito: FormControl = this.fb.control("", Validators.required);
   get favoritosArr() {
     return this.miFormulario.get('favorito') as  FormArray;
   }
@@ -23,6 +24,17 @@ export class DinamicosComponent {
   campoEsObligatorio() {
     return this.miFormulario.controls.nombre.invalid&& this.miFormulario.controls.nombre.touched;
   }
+
+  agregarFavorito() {
+    if(this.miFormulario.invalid){return;}
+    this.favoritosArr.push(new FormControl(this.nuevoFavorito.value, Validators.required))
+    this.nuevoFavorito.reset()
+  }
+
+  borrar(i:number) {
+    this.favoritosArr.removeAt(i);
+  }
+
   guardar() {
     if(this.miFormulario.invalid){
       this.miFormulario.markAllAsTouched();
