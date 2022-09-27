243d5b9b47f251696f33e080e5ba4f03e1211c44
Daareizag <diegoareizag32074818@gmail.com>
2022-09-26 22:06:03 -0300

    Validaciones de email mediante un servicio - Factorizacion de validaciones y errores personalizados

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { emailPattern, nombreApellidoPattern, noPuedeSerPrime } from 'src/app/shared/validators/validators';
+import { ValidatorsService } from '../../../shared/validators/validators.service';
+import { EmailValidatorsService } from '../../../shared/validators/email-validators.service';
+import { nombreApellidoPattern } from '../../../shared/validators/validators';
 
 @Component({
   selector: 'app-registro',
@@ -10,19 +12,48 @@ import { emailPattern, nombreApellidoPattern, noPuedeSerPrime } from 'src/app/sh
 })
 export class RegistroComponent implements OnInit {
 
-  miFormulario: FormGroup = this.fb.group({
-    nombre: ["", [Validators.required, Validators.pattern(nombreApellidoPattern)]],
-    email: ["", [Validators.required, Validators.pattern(emailPattern)]],
-    username: ["", [Validators.required, noPuedeSerPrime]],
-  })
+  mensaje:string='';
 
-  constructor(private fb: FormBuilder) { }
+    miFormulario: FormGroup = this.fb.group({
+        nombre: ['', [Validators.required, Validators.pattern(this.validationService.nombreApellidoPattern)]],
+        email: ['', [Validators.required, Validators.pattern(this.validationService.emailPattern)],[this.emailValidator]],
+        username: ['', [Validators.required, this.validationService.noPuedeSerPrime]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        password2: ['', [Validators.required]],
+      }, {
+        validators: [this.validationService.camposIguales('password', 'password2')]
+      })
+
+
+    emailErrorMsg(campo:string):string {
+    const errors = this.miFormulario.get(campo)?.errors;
+    if(errors?.required){
+      return 'El Email es obligatorio'
+    }else if(errors?.pattern){
+      return 'El Email no cumple con la estructura requerida'
+    }else if (errors?.emailTomado){
+      return 'El Email ya esta en uso'
+    }else if (errors?.noIguales ){
+      return 'Las contraseñas no coinciden'
+    }else if (errors?.noIguales ){
+      return 'Las contraseñas no coinciden'
+    }
+    return '';
+  }
+
+
+
+  constructor(private fb: FormBuilder,
+              private validationService: ValidatorsService,
+              private emailValidator: EmailValidatorsService) { }
 
   ngOnInit(): void {
     this.miFormulario.reset({
       nombre: 'Diego Areiza',
-      email: 'test1@text.com',
-      username: 'DtoPrax'
+      email: 'test1@test.com',
+      username: 'DtoPrax',
+      password: '123456',
+      password2: '123456'
     })
   }
 
@@ -33,5 +64,8 @@ export class RegistroComponent implements OnInit {
     console.log(this.miFormulario.value)
     this.miFormulario.markAllAsTouched();
   }
+
+
+
 
 }
--- src/app/shared/validators/validators.service.ts
+++ src/app/shared/validators/validators.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { ValidationError } from 'webpack';
+import { AbstractControl, FormControl, ValidationErrors } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ValidatorsService {
 
-  public  nombreApellidoPattern: string = '([a-zA-z]+) ([a-zA-z]+)';
-  public  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+  public nombreApellidoPattern: string = '([a-zA-z]+) ([a-zA-z]+)';
+  public emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
   constructor() { }
 
-    noPuedeSerPrime(control:FormControl): ValidationError | any{
+    noPuedeSerPrime(control:FormControl):ValidationErrors | null{
     const valor: string=control.value?.trim().toLowerCase();
     if(valor === 'prime'){
       return {
@@ -20,5 +19,17 @@ export class ValidatorsService {
       }
     }
       return null;
+   }
+   camposIguales(campo1: string, campo2: string) {
+    return (formGroup: AbstractControl): ValidationErrors | null => {
+      const pass1 = formGroup.get(campo1)?.value;
+      const pass2 = formGroup.get(campo2)?.value;
+      if(pass1 !== pass2){
+        formGroup.get(campo2)?.setErrors({noIgules: true})
+        return{noIguales: true}
+      }
+      formGroup.get(campo2)?.setErrors(null)
+      return null
+    }
    }
 }
